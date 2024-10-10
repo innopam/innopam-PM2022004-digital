5bff2346e25c0cba1720f816a5feb469a04305ca
PARK MINA <parkmina365@gmail.com>
2024-10-10 16:23:09 +0900

    Docker image build 방법 변경: .tar -> dockerfile

--- 02_Road_CD/start.sh
+++ 02_Road_CD/start.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 DIR=`dirname "$0"`
-docker load -i $DIR/opencd_pytorch.tar
+docker build -t opencd_pytorch:latest .
+# docker load -i $DIR/opencd_pytorch.tar
 docker-compose up
 # docker run -it -v $DIR/workspace:/workspace --gpus=all --ipc=host opencd_pytorch:latest
-
-# python /workspace/predict.py --output_path /workspace/out/out.gpkg
-# python /workspace/predict.py --output_path /workspace/out/out.gpkg --img_1 /workspace/sample_data/A --img_2 /workspace/sample_data/B
